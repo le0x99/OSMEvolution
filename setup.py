e8e8e168d3654ed53374ecc7fe96112e5527dcf2
Leonard D. Vorbeck <36996220+le0x99@users.noreply.github.com>
2022-05-09 21:05:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["pandas>=0.24.2", "requests>=2.22.0", "tqdm>=4.35.0", "osmapi>=1
 
 setup(
     name="OSMEvolution",
-    version="0.1",
+    version="1.0",
     author="Leonard Vorbeck",
     author_email="leomxyy@googlemail.com",
     description="A package for receiving and restructuring OSM historic data conveniently",
@@ -23,6 +23,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Intended Audience :: Developers',
